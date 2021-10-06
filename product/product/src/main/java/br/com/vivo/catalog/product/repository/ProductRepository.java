63791288b8db96957d9ce5616a8fa6817b6fccaa
jmarcosc <costa.joaomarcos@gmail.com>
2021-10-06 16:46:42 -0300

    Atualizado query para tratamento de valor null

--- product/product/src/main/java/br/com/vivo/catalog/product/repository/ProductRepository.java
+++ product/product/src/main/java/br/com/vivo/catalog/product/repository/ProductRepository.java
@@ -11,7 +11,7 @@ import br.com.vivo.catalog.product.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
 
-	@Query("SELECT p FROM Product p WHERE (p.price >= :minimumPrice AND p.price <= :maximumPrice) AND ( :query IS NULL OR UPPER(p.name) LIKE UPPER(CONCAT('%',:query,'%')) OR UPPER(p.description) LIKE UPPER(CONCAT('%',:query,'%')))")
+	@Query("SELECT p FROM Product p WHERE (:minimumPrice IS NULL OR p.price >= :minimumPrice) AND (:maximumPrice IS NULL OR p.price <= :maximumPrice) AND ( :query IS NULL OR UPPER(p.name) LIKE UPPER(CONCAT('%',:query,'%')) OR UPPER(p.description) LIKE UPPER(CONCAT('%',:query,'%')))")
 	public List<Product> getProductsByNameDescriptionAndPriceRange(@Param("minimumPrice") BigDecimal minimumPrice, 
 																   @Param("maximumPrice") BigDecimal maximumPrice, 
 																   @Param("query") String query);
